7b4a2e635877dd2cbba0d0223e0d746ce17e9b06
Ashish Kumar <ashishkumarg0877@gmail.com>
2021-11-28 20:08:44 +0530

    implemented login System

---
+++ authentication/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout
+
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request,'register.html',{'form':form})
+
+
+def login(request):
+    return render(request,'login.html')
+
+def logoutView(request):
+    logout(request)
+    return redirect('/')
+
